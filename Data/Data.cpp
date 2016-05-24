89baa91471f20858b083470cc5f740878ef65529
wispwisp <wisp@gcnet.ru>
2016-05-24 22:51:53 +0300

    change 'Data's types scope

--- Data/Data.cpp
+++ Data/Data.cpp
@@ -4,9 +4,9 @@
 #include <string>
 
 namespace Tools {
-  BitsType getBitsFromString(const std::string& buf) {
+  Data::BitsType getBitsFromString(const std::string& buf) {
 
-    BitsType bits(buf.size());
+    Data::BitsType bits(buf.size());
     for (size_t i = 0; i < buf.size(); ++i) {
       if (buf[i] == '0')
 	bits[i] = 0;
@@ -29,14 +29,14 @@ Data Data::parseUniqueDataFromStream(std::istream& stream) {
   if (not (stream >> buf))
     throw std::logic_error("Error reading stream");
 
-  size_t sizeOfDimension = buf.size();
+  data.sizeOfDimension = buf.size();
 
   // increment occurrence of transaction`s elements
   data.data[Tools::getBitsFromString(buf)]++;
 
   // II. process rest of the file content:
   while (stream >> buf) {
-    if (sizeOfDimension != buf.size())
+    if (data.sizeOfDimension != buf.size())
       throw std::logic_error("Data inconsistent");
 
     data.data[Tools::getBitsFromString(buf)]++;
--- Data/Data.hpp
+++ Data/Data.hpp
@@ -5,14 +5,19 @@
 #include <istream>
 #include <unordered_map>
 
-using BitsType = std::vector<bool>;
-using DataType = std::unordered_map<BitsType, size_t>;
-
 class Data {
-  DataType data;
 public:
+  using BitsType = std::vector<bool>;
+  using DataType = std::unordered_map<BitsType, size_t>;
+
   const DataType& get() const noexcept { return data; }
+  size_t transactionSize() const noexcept { return sizeOfDimension; }
+
   static Data parseUniqueDataFromStream(std::istream&);
+
+private:
+  size_t sizeOfDimension;
+  DataType data;
 };
 
 #endif // MY_DATA
